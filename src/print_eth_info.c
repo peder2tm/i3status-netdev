1779de0d796c7182d3fcddd4845e380c45dd44af
Peter Bjørn Jørgensen <peterbjorgensen@gmail.com>
2012-02-04 23:04:15 +0100

    Uuse /proc/net/dev on linux to print if speed

--- src/print_eth_info.c
+++ src/print_eth_info.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,7 +17,12 @@
 #include "i3status.h"
 
 #if defined(LINUX)
-#include <linux/ethtool.h>
+    #if defined(USE_PROC_NET_DEV)
+        static long long prev_recv_bytes, prev_sent_bytes, 
+        prev_nanoseconds = -1;
+    #else
+        #include <linux/ethtool.h>
+    #endif
 #include <linux/sockios.h>
 #define PART_ETHSPEED  "E: %s (%d Mbit/s)"
 #endif
@@ -34,19 +42,112 @@
 
 static int print_eth_speed(char *outwalk, const char *interface) {
 #if defined(LINUX)
-        /* This code path requires root privileges */
-        int ethspeed = 0;
-        struct ifreq ifr;
-        struct ethtool_cmd ecmd;
+        #if defined(USE_PROC_NET_DEV)
+            char buffer[4096], cad[256], *ni, *nf;
+            
+            // read network information
+            int fd = open("/proc/net/dev", O_RDONLY);
+            if (fd < 0) return;
+            int bytes = read(fd, buffer, sizeof(buffer)-1);
+            close(fd);
+            if (bytes < 0) return;
+            buffer[bytes] = 0;
 
-        ecmd.cmd = ETHTOOL_GSET;
-        (void)memset(&ifr, 0, sizeof(ifr));
-        ifr.ifr_data = (caddr_t)&ecmd;
-        (void)strcpy(ifr.ifr_name, interface);
-        if (ioctl(general_socket, SIOCETHTOOL, &ifr) == 0) {
-                ethspeed = (ecmd.speed == USHRT_MAX ? 0 : ecmd.speed);
-                return sprintf(outwalk, "%d Mbit/s", ethspeed);
-        } else return sprintf(outwalk, "?");
+            struct timespec tp;
+            clock_gettime(CLOCK_MONOTONIC, &tp);
+            long long nanoseconds = tp.tv_sec * 1000000000LL + tp.tv_nsec;
+
+            long long recv_bytes=LLONG_MAX, sent_bytes=LLONG_MAX;
+            int networkAvailable = 0;
+
+            // search for the proper network interface
+            strcpy(cad, interface);
+            strcat(cad, ":");
+            char *pif = strstr(buffer, cad);
+            if (pif != NULL) {
+                networkAvailable = 1;
+
+                // jump to the received bytes field
+                ni = pif + strlen(cad);
+                while (*ni && ((*ni == ' ') || (*ni == '\t'))) ni++;
+                for (nf = ni; *nf && (*nf != ' ') && (*nf != '\t'); nf++);
+                *nf++ = 0;
+
+                // get the received bytes
+                recv_bytes = atoll(ni);
+
+                // jump to the sent bytes field
+                        int skip;
+                for (skip = 0; skip < 8; skip++) {
+                    ni = nf;
+                    while (*ni && ((*ni == ' ') || (*ni == '\t'))) ni++;
+                    for (nf = ni; *nf && (*nf != ' ') && (*nf != '\t'); nf++);
+                    if (!*nf) break;
+                    *nf++ = 0;
+                }
+
+                // get the sent bytes
+                sent_bytes = atoll(ni);
+            }
+
+            //
+            // generate the result
+            strcpy(buffer, "");
+
+            int hasNet = networkAvailable && (prev_nanoseconds >= 0) && 
+                        (recv_bytes >= prev_recv_bytes) && (sent_bytes >= prev_sent_bytes);
+
+            if (!networkAvailable) {
+                sprintf(cad, "  %s is down", interface);
+                strcat(buffer, cad);
+            }
+            else if (!hasNet) {
+                strcat(buffer, "     ? kbps IN \n     ? kbps OUT");
+            }
+            else {
+                long long elapsed = nanoseconds - prev_nanoseconds;
+                if (elapsed < 1) elapsed = 1;
+                double seconds = elapsed / 1000000000.0;
+                long long sent = sent_bytes - prev_sent_bytes;
+                long long received = recv_bytes - prev_recv_bytes;
+                        // adding 999 ensures "1" for any rate above 0
+                long inbps = (long) (8 * received / seconds + 999); 
+                long outbps = (long) (8 * sent / seconds + 999);
+                if (inbps < 1000000)
+                    sprintf(cad, "DOWN: %6ld kbps, ", inbps/1000);
+                else
+                    sprintf(cad, "DOWN: %6.3f Mbps, ", inbps/1000000.0);
+                strcat(buffer, cad);
+
+                if (outbps < 1000000)
+                    sprintf(cad, "UP: %6ld kbps", outbps/1000);
+                else
+                    sprintf(cad, "UP: %6.3f Mbps", outbps/1000000.0);
+                strcat(buffer, cad);
+                
+            }
+
+            /* Save values for next iteration */
+            prev_recv_bytes = recv_bytes;
+            prev_sent_bytes = sent_bytes;
+            prev_nanoseconds = nanoseconds;
+
+            return sprintf(outwalk, buffer);
+        #else
+            /* This code path requires root privileges */
+            int ethspeed = 0;
+            struct ifreq ifr;
+            struct ethtool_cmd ecmd;
+
+            ecmd.cmd = ETHTOOL_GSET;
+            (void)memset(&ifr, 0, sizeof(ifr));
+            ifr.ifr_data = (caddr_t)&ecmd;
+            (void)strcpy(ifr.ifr_name, interface);
+            if (ioctl(general_socket, SIOCETHTOOL, &ifr) == 0) {
+                    ethspeed = (ecmd.speed == USHRT_MAX ? 0 : ecmd.speed);
+                    return sprintf(outwalk, "%d Mbit/s", ethspeed);
+            } else return sprintf(outwalk, "?");
+        #endif
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
         char *ethspeed;
         struct ifmediareq ifm;
